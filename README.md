bcca348aa0f7fc2cc344dbb5798df7e5f6e3ec4b
ArantesVini <vinicius.daniel02@gmail.com>
2023-06-16 18:52:38 -0300

    feat: add the create table script and dw modelling in readme

--- README.md
+++ README.md
@@ -14,7 +14,37 @@ The objective of this project is to implement an ETL process that reads CSV file
 
 ## Repository Structure
 
+- `/database/` the SQL scripts used to create the DW;
 - TODO add the repository strutucre
+
+## About the project
+
+Consider the questions below from a Logistics Manager in a retail network:
+• How many deliveries are made per month?
+• Out of the total number of deliveries per month, how many are on time?
+• What is the percentage of on-time deliveries versus delayed deliveries?
+• Which months had the highest number of on-time deliveries?
+• Did a specific customer receive more deliveries on time or delayed?
+
+These are typical business questions that managers seek answers to. To answer these and other questions, we will build a Data Warehouse (DW). However, let's automate the DW loading process from the data source in CSV format. Our ETL (Extract, Transform, Load) process will verify the data, perform cleaning tasks, remove inconsistencies, prepare the schema, and then load it into the DW. As the icing on the cake, we will create SQL queries to answer some business questions.
+
+All the data used in this project are fictional, for practice and research purposes only
+
+## DataWarehouse modelling
+
+### Dimensions Tables
+
+- D_CUSTOMER;
+- D_SHIPP_COMPANY;
+- D_PRODUCT;
+- D_DATE;
+- D_WAREHOUSE;
+- D_DELIVERY;
+- D_PAYMENT;
+
+### Fact Table
+
+- **F_LOGISTIC**
 
 ## License
 
---
+++ databse/create_tables.sql
@@ -0,0 +1,107 @@
+CREATE TABLE dbs."D_CUSTOMER"
+(
+    customer_id int NOT NULL,
+    customer_name text,
+    customer_surname text,
+    PRIMARY KEY (customer_id)
+);
+
+
+CREATE TABLE dbs."D_SHIPP_COMPANY"
+(
+    shippcom_id integer NOT NULL,
+    shippcom_name text,
+    PRIMARY KEY (shippcom_id)
+);
+
+
+CREATE TABLE dbs."D_WAREHOUSE"
+(
+    warehouse_id bigint NOT NULL,
+    warehouse_name text,
+    PRIMARY KEY (warehouse_id)
+);
+
+
+CREATE TABLE dbs."D_DELIVERY"
+(
+    delivery_id bigint NOT NULL,
+    delivery_adress text,
+    delivery_country text,
+    delivery_type text,
+    PRIMARY KEY (delivery_id)
+);
+
+
+CREATE TABLE dbs."D_PAYMENT"
+(
+    payment_id bigint NOT NULL,
+    payment_type text,
+    PRIMARY KEY (payment_id)
+);
+
+
+CREATE TABLE dbs."D_DATE"
+(
+    date_id bigint NOT NULL,
+    date_full text,
+    date_day integer,
+    date_month integer,
+    date_year integer,
+    PRIMARY KEY (date_id)
+);
+
+
+CREATE TABLE dbs."F_LOGISTIC"
+(
+    customer_id integer,
+    shippcom_id integer,
+    warehouse_id integer,
+    delivery_id integer,
+    payment_id integer,
+    date_id integer,
+    shipping_value double precision,
+    PRIMARY KEY (customer_id, shippcom_id, warehouse_id, delivery_id, payment_id, date_id)
+);
+
+
+ALTER TABLE IF EXISTS dbs."F_LOGISTIC"
+    ADD CONSTRAINT "FK_CUSTOMER" FOREIGN KEY (customer_id)
+    REFERENCES dbs."D_CUSTOMER" (customer_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS dbs."F_LOGISTIC"
+    ADD CONSTRAINT "FK_SHIPPCOM" FOREIGN KEY (shippcom_id)
+    REFERENCES dbs."D_SHIPP_COMPANY" (shippcom_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS dbs."F_LOGISTIC"
+    ADD CONSTRAINT "FK_WAREHOUSE" FOREIGN KEY (warehouse_id)
+    REFERENCES dbs."D_WAREHOUSE" (warehouse_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS dbs."F_LOGISTIC"
+    ADD CONSTRAINT "FK_DELIVERY" FOREIGN KEY (delivery_id)
+    REFERENCES dbs."D_DELIVERY" (delivery_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS dbs."F_LOGISTIC"
+    ADD CONSTRAINT "FK_PAYMENT" FOREIGN KEY (payment_id)
+    REFERENCES dbs."D_PAYMENT" (payment_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
+
+
+ALTER TABLE IF EXISTS dbs."F_LOGISTIC"
+    ADD CONSTRAINT "FK_DATE" FOREIGN KEY (date_id)
+    REFERENCES dbs."D_DATE" (date_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION;
