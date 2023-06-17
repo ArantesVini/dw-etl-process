11945ac428a0c7d8756683e58fd22e1e3b491fae
ArantesVini <vinicius.daniel02@gmail.com>
2023-06-17 10:12:57 -0300

    feat: add the initial data and first task in the dag

--- dags/real_time_etl.py
+++ dags/real_time_etl.py
@@ -8,3 +8,33 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.utils.dates import days_ago
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+def extarct_csv_files():
+    values = []
+    with open('data/D_CUSTOMER.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            values.append(tuple(row))
+
+
+with DAG(
+        dag_id='real_time_etl',
+        default_args=default_args,
+        schedule_interval='0 0 * * *',
+        dagrun_timeout=timedelta(minutes=60),
+        description='ETL Job to load data in a DW with Airflow',
+        start_date=airflow.utils.dates.days_ago(1),) as dag:
+
+    read_csv = PythonOperator(
+        task_id='read_csv',
+        python_callable=extarct_csv_files,
+    )
